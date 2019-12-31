2f61e346dc7eb973277882e856fc296097d61950
jamil4321 <jamil.ahmed.ja28@mail.com>
2019-12-31 10:51:37 +0500

    go to windows

--- js/login.js
+++ js/login.js
@@ -1,16 +1,50 @@
-
-    let hash = calcSHA1('Jamil')
-    console.log(hash)
-
-    const db = firebase.database();
-
-    function addUser() {
-        let userId = db.ref().child('posts').push().key;
-        let userName = document.getElementById('userName').value
-        let userPass = document.getElementById('userPass').value
-        userPass = calcSHA1(userPass)
-        firebase.database().ref('users/' + userId).set({
-          userName: userName,
-          userPass: userPass,
-        });
+const db = firebase.database();
+function addUser() {
+  let userId = db
+    .ref()
+    .child("posts")
+    .push().key;
+  let userName = document.getElementById("userName").value;
+  let userPass = document.getElementById("userPass").value;
+  //   userPass = calcSHA1(userPass);
+  db.ref("users/" + userId).set({
+    userName: userName,
+    userPass: userPass
+  });
+}
+var cokie;
+function login() {
+  let userName = document.getElementById("userName").value.toLowerCase();
+  let userPass = document.getElementById("userPass").value;
+  let flag = false;
+  let users = [];
+  db.ref("users/").on("value", function(snapshot) {
+    // console.log(snapshot.val())
+    users = Object.values(snapshot.val());
+    for (var i = 0; i < users.length; i++) {
+      if (users[i].userName === userName && users[i].userPass === userPass) {
+        flag = true;
+        cokie = document.cookie = "userName = " +userName; 
       }
+    }
+    if (flag) {
+        location.pathname = "/index.html";
+    } else {
+      let alert = document.getElementById("alert");
+      alert.innerHTML = `This is a danger alert—check it out!`;
+      setTimeout(()=>alert.innerHTML="",3000)
+    }
+  });
+}
+if(!cokie){
+    console.log(body,cokie)
+  }else{
+    body.innerHTML="Kindly Login First"
+
+    location.pathname ="/Login.html"
+  }
+
+function logout(){
+  document.cookie = "userName = ;expires=Thu, 01 Jan 1970 00:00:00 UTC;"
+}
+
