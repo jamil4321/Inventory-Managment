913b23d49bcf88d511b6cd121c71d4c99044fb7b
jamil4321 <jamil.ahmed.ja28@mail.com>
2020-01-14 17:16:26 +0500

    eduited PO

---
+++ js/addPO.js
@@ -0,0 +1,36 @@
+let sel_supplier = document.getElementById("sel-suppleir");
+let supName = document.getElementById("supplierName");
+let supNumber = document.getElementById("supplierNumber");
+let supAdd = document.getElementById("supplierAddress");
+let supplier = [];
+db.ref("supplier/").once("value", data => {
+  supplier = Object.values(data.val());
+});
+function addPO() {
+  sel_supplier.innerHTML = "";
+  var option = document.createElement("option");
+  option.text = "--Select Supplier--";
+  sel_supplier.appendChild(option);
+  for (let i = 0; i < supplier.length; i++) {
+    var option = document.createElement("option");
+    option.value = supplier[i].supplierName;
+    option.text = supplier[i].supplierName;
+    sel_supplier.appendChild(option);
+  }
+  
+}
+function startPO() {
+  supName.disabled = true;
+  supNumber.disabled = true;
+  supAdd.disabled = true;
+  let addPOBTN = document.getElementById("addPO");
+  addPOBTN.disabled = true;
+  for (let i = 0; i < supplier.length; i++) {
+    if (sel_supplier.value === supplier[i].supplierName) {
+      supName.value = supplier[i].supplierName;
+      supNumber.value = supplier[i].supplierNumber;
+      supAdd.value = supplier[i].supplierAddress;
+      break;
+    }
+  }
+}
--- js/extraFeature.js
+++ js/extraFeature.js
@@ -13,3 +13,20 @@ function toggle() {
     console.log(mainContent.className)
   }
 }
+
+
+
+// let slider = document.getElementById('slider')
+// let mainContent = document.getElementById('main-content')
+// function toggle() {
+//   if (slider.className === 'col-xs-12 col-sm-12 col-md-3 col-lg-2 slider bg-dark position-fixed') {
+//     slider.className = 'slider-display'
+//     mainContent.className = 'col'
+//     console.log(slider.className)
+//   }
+//   else {
+//     slider.className = 'col-xs-12 col-sm-12 col-md-3 col-lg-2 slider bg-dark position-fixed'
+//     mainContent.className = 'col offset-2'
+//     console.log(mainContent.className)
+//   }
+// }
