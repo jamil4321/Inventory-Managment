8fbfdc5048751b3d1c9645fa317f7268c933b835
jamil4321 <jamil.ahmed.ja28@mail.com>
2020-01-15 20:32:14 +0500

    left this project for some time

--- js/addPO.js
+++ js/addPO.js
@@ -2,12 +2,19 @@ let sel_supplier = document.getElementById("sel-suppleir");
 let supName = document.getElementById("supplierName");
 let supNumber = document.getElementById("supplierNumber");
 let supAdd = document.getElementById("supplierAddress");
+let sel_item = document.getElementById('sel-item')
 let supplier = [];
+let items = [];
 let addPOBTN = document.getElementById("addPO");
 db.ref("supplier/").on("value", data => {
   supplier = Object.values(data.val());
 });
-
+db.ref("item/").on("value", data => {
+  items = Object.values(data.val());
+  for (let i = 0;i < items.length; i++){
+    itemName.push(items[i].itemName) 
+   }
+});
 
 function addPO() {
   sel_supplier.innerHTML = "";
@@ -15,7 +22,7 @@ function addPO() {
   option.text = "--Select Supplier--";
   sel_supplier.appendChild(option);
   for (let i = 0; i < supplier.length; i++) {
-    var option = document.createElement("option");
+    option = document.createElement("option");
     option.value = supplier[i].supplierName;
     option.text = supplier[i].supplierName;
     sel_supplier.appendChild(option);
@@ -34,7 +41,34 @@ function startPO() {
       break;
     }
   }
-  addPOBTN.classList += ' disabled'
+  addPOBTN.classList += " disabled";
 }
-function addItem() {
+function findItem() {
+  sel_item.innerHTML = "";
+  var option = document.createElement("option");
+  option.text = "--Select Supplier--";
+  sel_item.appendChild(option);
+  for (let i = 0; i < items.length; i++) {
+    if (sel_supplier.value === items[i].supplier) {
+      option = document.createElement("option");
+      option.value = items[i].itemName;
+      option.text = items[i].itemName;
+      sel_item.appendChild(option);
+    }
+  }
 }
+
+
+// Find Item via search box
+let search = document.getElementById('searchItem')
+let itemName = [];
+search.addEventListener('keyup',(e)=>{
+  let key = e.target.value.toLowerCase()
+  itemName.forEach((item)=>{
+    if(item.indexOf(key) != -1){
+      console.log(item)
+    }else{
+      console.log('no item found')
+    }
+  })
+})
